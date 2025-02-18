f2a1f8c4aeee3632d6c70c9a94cab315ba538824
satoshisong01 <rudtjrch@naver.com>
2025-02-18 14:36:47 +0900

    10시 5분에 해둠 스케줄러

--- scheduler.js
+++ scheduler.js
@@ -8,7 +8,7 @@ const AUTOMATION_API_URL = "http://3.34.144.172:3000/api/automation/start";
 
 // 서버가 UTC 기준이라면, 한국 시간 오전 10시는 UTC 오전 1시이므로,
 // 크론 표현식을 "0 1 * * *"로 설정합니다.
-cron.schedule("35 5 * * *", async () => {
+cron.schedule("5 1 * * *", async () => {
   console.log("start scheduler.js", new Date());
   console.log("스케줄러 실행: 자동화 작업을 시작합니다.");
   try {
